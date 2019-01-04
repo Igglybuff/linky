0cc6582094655752c50e955f7ad3563844d269d6
Igglybuff <ed@edflix.club>
2019-01-04 11:11:19 +0000

    second commit

--- linky/scripts/linky.py
+++ linky/scripts/linky.py
@@ -1,7 +1,7 @@
 import click
 
 @click.command()
-@click.option('-l', '--link', 'url', required=True)
+@click.option('-l', '--link', 'url', required=True, help='The URL to a file on a premium host that you want to download.')
 @click.option('-c', '--config', 'config', envvar='LINKY_CONFIG_PATH', required=True, default='~/.config/linky/linky.conf', show_default=True, help='The path to your Linky configuration file.')
 def linky(url, config):
     pass
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='linky',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        linky=linky.scripts.linky:linky
+    ''',
+)
