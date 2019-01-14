bab8b3b86a52352f57d6a5d28aca17d5bd04c6d7
Igglybuff <ed@edflix.club>
2019-01-14 21:36:06 +0000

    more bones for statuschecker

--- linky/statuscheck.py
+++ linky/statuscheck.py
@@ -8,10 +8,16 @@ class StatusChecker:
         self.download_client = download_client
 
     def get_status(self, links=None, all_items=None):
+        if str(self.download_client).lower() == 'jdownloader':
+            self.get_jdownloader_status(links)
+        elif str(self.download_client).lower() == 'pyload':
+            self.get_pyload_status(links)
+        else:
+            print('ERROR: Something went wrong checking the status of your link(s).')
+            exit(1)
+
+    def get_jdownloader_status(self, links):
         pass
 
-    def get_jdownloader_status(self):
-        pass
-
-    def get_pyload_status(self):
+    def get_pyload_status(self, links):
         pass
